9fcf9daa89cb307b725f82970bd30846f5928cd2
chirag gupta <chiragg1106@gmail.com>
2022-08-09 21:09:37 +0530

    initial page setup

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ViewChatBot;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ViewChatBot::class, 'page'] )->name('initial-page');
+
